29c701cb37645e0412065279c40ecdd012c042b3
umairbeig <umairbeig@gmail.com>
2023-05-15 10:26:43 +0530

    dropdown

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [word, setWord] = useState('');
+  const [selectedValue, setSelectedValue] = useState('a');
   const [responses, setResponses] = useState([]);
 
   const handleWordChange = (event) => {
     setWord(event.target.value);
   };
 
+  const handleDropdownChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await fetch(`http://localhost:8080/api/search/${word}`);
+      const response = await fetch(`http://localhost:8080/api/search/${word}/${selectedValue}`);
       const data = await response.json();
+     
       setResponses(data);
-      console.log(data);
+      
+     
+      // console.log(data);
     } catch (error) {
       console.error('Error:', error);
+      console.log("word not found")
     }
   };
 
@@ -41,6 +50,11 @@ const App = () => {
       <h1>Your App Component</h1>
       <form onSubmit={handleSubmit}>
         <input type="text" value={word} onChange={handleWordChange} />
+        <select value={selectedValue} onChange={handleDropdownChange}>
+          <option value="table1">table1</option>
+          <option value="table2">table2</option>
+          <option value="table3">table3</option>
+        </select>
         <button type="submit">Search</button>
       </form>
       <div>
